2b9bf072d542b2e7a3bb3b72ab417ffcd6d63340
Victoria0292 <tlynn0292@gmail.com>
2022-11-09 15:36:34 -0500

    add comments explaining script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+//Thanks to study sqad academy on YouTube for the very informative video.
+
+//querySelectorAll will select all my cards not just one
 const cards = document.querySelectorAll('.card')
 
+//Loops through each card
 cards.forEach(card => {
+    //Listening for a click, when click happens function will run
     card.addEventListener('click', () => {
         removeActiveClasses()
         card.classList.add('active')
     })
 })
 
+//Removes the classes that are not active
 function removeActiveClasses() {
+    //Loops through each card
     cards.forEach(card => {
         card.classList.remove('active')
     })
